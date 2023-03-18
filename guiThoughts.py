dd7d623c1db4cd29aa2c184149363d2604fbcb75
Tyler Poirier <tyler.k.poirier@gmail.com>
2023-03-18 14:41:29 -0400

    GUI Basic Layout
    The GUI now shows a basic layout with placeholder tkinter widgets

--- guiThoughts.py
+++ guiThoughts.py
@@ -89,9 +89,6 @@ root.title("Modeling The Thinking")
 # set window with size 800x600 (4:3) (maybe add option later that lets user change default opening res)
 root.geometry("800x600")
 
-testlabel = Label(text='test')
-testlabel.pack(side = BOTTOM)
-
 # add menubar to window
 menu()
 
@@ -100,18 +97,53 @@ menu()
 # add paned window to right hand side of root for diagrams
 # diagramPane()
 
-# paned window
-pw = ttk.PanedWindow(orient=BOTH)
+# Main paned window, oriented horizontal to stack items left and right
+pw = ttk.PanedWindow(orient='horizontal')
 
-# Left listbox
-left_list = Listbox(root)
-left_list.pack(side=LEFT)
-pw.add(left_list)
+# left paned window to hold three widgets on top of one another
+lpw = ttk.PanedWindow(orient='vertical', width=200)
 
-# Right listbox
-right_list = Listbox(root)
+# Right paned window to hold main diagram viewer and info at bottom
+rpw = ttk.PanedWindow(orient='vertical')
+
+# Create basic listboxes to hold leftside elements
+# Later will be different types of tkinter widgets. https://tkdocs.com/tutorial/widgets.html
+# The height of listboxes is number of text elements to show, not pixels
+top_lb = Listbox(root, height=8)
+top_lb.insert(1, "Tools to use")
+top_lb.insert(2, "  Drag and Move Tool")
+top_lb.insert(3, "  Add Relations")
+top_lb.insert(4, "  Select a bunch at once tool")
+top_lb.insert(5, "  etc")
+top_lb.pack()
+lpw.add(top_lb)
+
+middle_lb = Listbox(root, height=8)
+middle_lb.insert(1, "Drag and drop area to simply")
+middle_lb.insert(2, "click type of tool to add to dia")
+middle_lb.pack()
+lpw.add(middle_lb)
+
+bottom_lb = Listbox(root)
+bottom_lb.insert(1, "List of stuff added, maybe in")
+bottom_lb.insert(2, "Photoshop layer style")
+bottom_lb.insert(3, "Will likely want ttk Treeview")
+bottom_lb.pack()
+lpw.add(bottom_lb)
+
+lpw.pack(fill=BOTH, expand=True)
+pw.add(lpw)
+
+# Right listbox to make up entire right side of window
+right_list = Listbox(root, height=35)
 right_list.pack(side=RIGHT)
-pw.add(right_list)
+rpw.add(right_list)
+
+right_text = Label(text="Additional Info (count of entities, etc) ?")
+right_text.pack(side=RIGHT)
+rpw.add(right_text)
+rpw.pack(fill=BOTH, expand=True)
+pw.add(rpw)
 
 # place the panedwindow on the root window
 pw.pack(fill=BOTH, expand=True)
