742ac5b710a6bf4b2bc508986efac1a6d0515074
Tim <tperr916@yahoo.com>
2023-03-25 13:27:58 -0400

    Some math stuff (work to be done)

--- cmlmath.py
+++ cmlmath.py
@@ -8,6 +8,7 @@ from cmltokens import TokenType
 from cmlparser import CMLParser
 from cmlclasses import ModelValue
 from cmldimension import dim_mul, dim_div, dim_equal
+import math
 
 
 def handle_math_function(parser: CMLParser,
@@ -57,6 +58,8 @@ def handle_math_function(parser: CMLParser,
 
 
 def star(parser, func_id, args):
+    assert len(args) < 2,\
+           "Improper number of args for star operation, must be at least 2"
     result = args.pop(0)
     assert result[0] == TokenType.MODEL_VALUE and\
            isinstance(result[1], ModelValue),\
@@ -70,51 +73,285 @@ def star(parser, func_id, args):
         result[1].dimension = dim_mul(result.dimension, val.dimension)
     return result
 
-
 def divide(parser, func_id, args):
+    assert len(args) < 2,\
+           "Improper number of args for divide operation, must be at least 2"
     result = args.pop(0)
     assert result[0] == TokenType.MODEL_VALUE and\
            isinstance(result[1], ModelValue),\
-           "Improper value in star operation."
-    assert len(args) == 2,\
-           "Arg count for divide op must be 2"
-    val = args.pop(0)
-    assert val[0] == TokenType.MODEL_VALUE and\
-           isinstance(val[1], ModelValue),\
-           "Improper value in div operation."
-    result[1].quantity /= val.quantity
-    result[1].dimension = dim_div(result.dimension, val.dimension)
+           "Improper value in divide operation."
+    while len(args) > 0:
+        val = args.pop(0)
+        assert val[0] == TokenType.MODEL_VALUE and\
+            isinstance(val[1], ModelValue),\
+            "Improper value in div operation."
+        result[1].quantity /= val.quantity
+        result[1].dimension = dim_div(result.dimension, val.dimension)
+
     return result
 
-
 def add(parser, func_id, args):
+    assert len(args) < 2,\
+           "Improper number of args for add operation, must be at least 2"
     result = args.pop(0)
     assert result[0] == TokenType.MODEL_VALUE and\
            isinstance(result[1], ModelValue),\
-           "Improper value in star operation."
+           "Improper value in add operation."
     while len(args) > 0:
         val = args.pop(0)
         assert val[0] == TokenType.MODEL_VALUE and\
                isinstance(val[1], ModelValue),\
-               "Improper value in star operation."
+               "Improper value in add operation."
         result[1].quantity += val.quantity
         assert dim_equal(result[1].dimension, val[1].dimension),\
                "Dimension in add op are not equal."
     return result
 
-
 def sub(parser, func_id, args):
+    assert len(args) < 2,\
+           "Improper number of args for sub operation, must be at least 2"
     result = args.pop(0)
     assert result[0] == TokenType.MODEL_VALUE and\
            isinstance(result[1], ModelValue),\
-           "Improper value in star operation."
-    assert len(args) == 2,\
-           "Arg count for divide op must be 2"
-    val = args.pop(0)
-    assert val[0] == TokenType.MODEL_VALUE and\
-           isinstance(val[1], ModelValue),\
-           "Improper value in div operation."
-    result[1].quantity -= val.quantity
-    assert dim_equal(result[1].dimension, val[1].dimension),\
-           "Dimension in add op are not equal."
+           "Improper value in sub operation."
+    while len(args) > 0:
+        val = args.pop(0)
+        assert val[0] == TokenType.MODEL_VALUE and\
+            isinstance(val[1], ModelValue),\
+            "Improper value in div operation."
+        result[1].quantity -= val.quantity
+        assert dim_equal(result[1].dimension, val[1].dimension),\
+            "Dimension in sub op are not equal."
+    return result
+
+def abs(parser, func_id, args):
+    assert len(args) != 1,\
+           "Improper number of args for abs operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in abs operation."
+
+    result[1].quantity = abs(result[1].quantity)
+    return result
+
+def acos(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for acos operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in acos operation."
+
+    result[1].quantity = math.acos(result[1].quantity)
+    return result
+
+def acosh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for acosh operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in acosh operation."
+
+    result[1].quantity = math.acosh(result[1].quantity)
+    return result
+
+def asin(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for asin operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in asin operation."
+
+    result[1].quantity = math.asin(result[1].quantity)
+    return result
+
+def asinh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for asinh operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in asinh operation."
+
+    result[1].quantity = math.asinh(result[1].quantity)
+    return result
+
+def atan(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for atan operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in atan operation."
+
+    result[1].quantity = math.atan(result[1].quantity)
+    return result
+
+def atanh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for atanh operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in atanh operation."
+
+    result[1].quantity = math.atanh(result[1].quantity)
+    return result
+
+def cos(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for cos operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in cos operation."
+
+    result[1].quantity = math.cos(result[1].quantity)
+    return result
+
+def cosh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for cosh operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in cosh operation."
+
+    result[1].quantity = math.cosh(result[1].quantity)
+    return result
+
+def exp(parser, func_id, args):
+    pass
+def expt(parser, func_id, args):
+    pass
+
+def log(parser, func_id, args):
+    pass
+
+def max(parser, func_id, args):
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+        isinstance(result[1], ModelValue),\
+        "Improper value in max operation."
+    
+    while len(args) > 0:
+        current = args.pop(0)
+        assert result[0] == TokenType.MODEL_VALUE and\
+            isinstance(result[1], ModelValue),\
+            "Improper value in max operation."
+        if current > result:
+            result = current
+    return result
+
+def min(parser, func_id, args):
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+        isinstance(result[1], ModelValue),\
+        "Improper value in min operation."
+    
+    while len(args) > 0:
+        current = args.pop(0)
+        assert result[0] == TokenType.MODEL_VALUE and\
+            isinstance(result[1], ModelValue),\
+            "Improper value in min operation."
+        if current < result:
+            result = current
+    return result
+
+def mod(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for tan operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in tan operation."
+
+    result[1].quantity = math.tan(result[1].quantity)
+    return result
+
+def sin(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for sin operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in sin operation."
+
+    result[1].quantity = math.sin(result[1].quantity)
+    return result
+
+def sinh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for sinh operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in sinh operation."
+
+    result[1].quantity = math.sinh(result[1].quantity)
+    return result
+
+def sqrt(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for sqrt operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in sqrt operation."
+
+    result[1].quantity = math.sqrt(result[1].quantity)
+    return result
+
+def tan(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for tan operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in tan operation."
+
+    result[1].quantity = math.tan(result[1].quantity)
+    return result
+
+def tanh(parser, func_id, args):
+    #TODO dimension stuffs
+    assert len(args) != 1,\
+           "Improper number of args for acos operation, must be 1"
+    
+    result = args.pop(0)
+    assert result[0] == TokenType.MODEL_VALUE and\
+           isinstance(result[1], ModelValue),\
+           "Improper value in acos operation."
+
+    result[1].quantity = math.tanh(result[1].quantity)
     return result
